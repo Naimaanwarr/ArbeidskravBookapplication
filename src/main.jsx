ba8de34697e2ac2457c2acfca8b300b9fede7a5e
Naimaanwarr <naimaanwarr@gmail.com>
2024-10-26 21:22:05 +0200

    submit new book to server

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,67 @@
-import React     from "react";
+import React from "react";
 import {createRoot} from "react-dom/client";
+import { useState } from "react";
 
 const root = createRoot(document.getElementById("root"));
 
-function BookForm() {
-    return <form>
+function BookForm({onNewAdd}) {
+
+    const [title, setTitle] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        onNewAdd({title})
+        setTitle("");
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <div>
         <label>
-            Book Title: <input type="text"/>
+            Book Title: <input
+            type="text"
+            value={title}
+            onChange={(e) =>
+                setTitle(e.target.value)}
+        />
         </label>
+        </div>
         <button>Submit</button>
     </form>;
 }
 
 function LibraryApplication() {
+    const [books, setBooks] = useState([]);
+
+
+   async function handleNewAdd(book) {
+        setBooks((prevBooks) => [book, ...prevBooks]);
+
+
+    await fetch("/api/books", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Books),
+    })
+}
+
     return <>
         <h1>Books i want to read</h1>
 
-        <BookForm/>
+        <ul>
+            {books.map((book, index) => (
+                <div key={index}>
+                    <input type="checkbox"/>
+                    {book.title}
+                </div>
+            ))}
+        </ul>
 
-        </>;
+
+        <BookForm onNewAdd={handleNewAdd}/>
+
+    </>;
 
 
 }
